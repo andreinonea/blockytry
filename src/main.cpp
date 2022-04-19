7d0fabc7b7ed00f6a18312387abdf2a05d5d60a7
Andrei N. Onea <andrei@fratiionea.ro>
2022-04-19 22:12:50 +0300

    imgui: integrate sample code

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,10 @@
 
 #include <spdlog/spdlog.h>
 
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+#include <imgui/implot.h>
 #define GLFW_INCLUDE_NONE
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
@@ -620,6 +624,7 @@ int main (int argc, char **argv)
     glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     // Create a windowed mode window and make its context current
     int width = 1024;
@@ -662,6 +667,41 @@ int main (int argc, char **argv)
     // glfwSetCharCallback(window, character_callback);
     glfwSetKeyCallback (window, key_callback);
     glfwSetCursorPosCallback (window, mouse_callback);
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION ();
+    ImGui::CreateContext ();
+    ImPlot::CreateContext();
+    ImGuiIO& io = ImGui::GetIO (); (void) io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark ();
+    //ImGui::StyleColorsClassic ();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL (window, true);
+    ImGui_ImplOpenGL3_Init ("#version 330");
+
+    // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Read 'docs/FONTS.md' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    //io.Fonts->AddFontDefault ();
+    //io.Fonts->AddFontFromFileTTF ("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF ("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+    //io.Fonts->AddFontFromFileTTF ("../../misc/fonts/DroidSans.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF ("../../misc/fonts/ProggyTiny.ttf", 10.0f);
+    //ImFont* font = io.Fonts->AddFontFromFileTTF ("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese ());
+    //IM_ASSERT (font != NULL);
+
+    // Our state
+    bool show_demo_window = true;
+    bool show_another_window = false;
 
     // Setup cube geometry.
     const glm::vec3 caca (0.0f, 0.0f, 0.0f);
@@ -755,6 +795,8 @@ int main (int argc, char **argv)
     // Loop until the user closes the window
     while (! glfwWindowShouldClose (window))
     {
+        glfwPollEvents ();
+
         // FOST_LOG_INFO ("Frame debug: {}ms dt | {} fps", fost::runtime::frametime ().count (), fost::runtime::fps ());
         // IMPORTANT! Must cycle runtime to advance simulation (calculates delta time).
         fost::runtime::cycle ();
@@ -779,6 +821,52 @@ int main (int argc, char **argv)
             accumulator -= fost::runtime::tick_unit;
         }
 
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame ();
+        ImGui_ImplGlfw_NewFrame ();
+        ImGui::NewFrame ();
+
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow (&show_demo_window);
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin ("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text ("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox ("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox ("Another Window", &show_another_window);
+
+            ImGui::SliderFloat ("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+
+            if (ImGui::Button ("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine ();
+            ImGui::Text ("counter = %d", counter);
+
+            ImGui::Text ("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO ().Framerate, ImGui::GetIO ().Framerate);
+            ImGui::End ();
+        }
+
+        // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin ("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text ("Hello from another window!");
+            if (ImGui::Button ("Close Me"))
+                show_another_window = false;
+            ImGui::End ();
+        }
+
+        ImPlot::ShowDemoWindow ();
+
+        // Rendering
+        ImGui::Render ();
+
         static const GLfloat background_color[] = { 0.2f, 0.2f, 0.2f, 1.0f };
         glClearBufferfv (GL_COLOR, 0, background_color);
 
@@ -800,9 +888,15 @@ int main (int argc, char **argv)
 
         glDrawElements (GL_TRIANGLE_STRIP, 14, GL_UNSIGNED_INT, nullptr);
 
+        ImGui_ImplOpenGL3_RenderDrawData (ImGui::GetDrawData ());
+
         glfwSwapBuffers (window);
-        glfwPollEvents ();
     }
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown ();
+    ImGui_ImplGlfw_Shutdown ();
+    ImPlot::DestroyContext ();
+    ImGui::DestroyContext ();
 
     glDeleteProgram (prog);
     glDeleteVertexArrays (1, &vao);
